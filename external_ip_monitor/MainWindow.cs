2e4a5aaee3e38a26ca7bc5696d8ca079e1ab50e0
Michał Krawczyk <m.kr.krawczyk@gmail.com>
2016-12-15 21:55:09 +0100

    Rozszerzanie pulpitu na Linuxie + screenshoty odpowiedniej czesci ekranu

--- external_ip_monitor/MainWindow.cs
+++ external_ip_monitor/MainWindow.cs
@@ -19,9 +19,14 @@ public partial class MainWindow : Gtk.Window
 
 	protected void OnBtnStartClicked(object sender, EventArgs e)
 	{
-		Bitmap bmpScreenshot = new Bitmap(Screen.Width, Screen.Height);
+		//Rozszerzanie ekranu - obydwie linie potrzebne bo bez pierwszej rozjezdza sie ekran
+		//xrandr --fb 3840x1080 --output VGA-1 --paning 3840x1080+0+0
+		//xrandr --fb 3840x1080 --output VGA-1 --paning 1920x1080+0+0
+		//Powrot
+		//xrandr -s 1920x1080
+		Bitmap bmpScreenshot = new Bitmap(1920, 1080);
 		Graphics g = Graphics.FromImage(bmpScreenshot);
-		g.CopyFromScreen(1920, 0, 0, 0, new Size(1920, 1080));
+		g.CopyFromScreen(1920, 0, 0, 0, bmpScreenshot.Size);
 
 		bmpScreenshot.Save("test.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
 		//external_ip_monitor.Server.StartListening();
