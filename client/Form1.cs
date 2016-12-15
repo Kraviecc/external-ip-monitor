a1ac6018d36d3cf5d7a910e89634b6528a474c77
Michal Krawczyk <m.kr.krawczyk@gmail.com>
2016-12-15 19:12:19 +0100

    dodano projekt klienta windowsowy, gitignore

---
+++ client/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace client
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            //AsynchronousClient.StartClient();
+            
+            Bitmap bmpScreenshot = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            Graphics g = Graphics.FromImage(bmpScreenshot);
+            g.CopyFromScreen(0, 0, 0, 0, Screen.PrimaryScreen.Bounds.Size);
+
+            MemoryStream memoryStream = new MemoryStream();
+            bmpScreenshot.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Jpeg);
+
+
+            ImageForm imageForm = new client.ImageForm();
+            imageForm.pictureBox.Image = Image.FromStream(memoryStream);
+
+            imageForm.Show();
+        }
+    }
+}
